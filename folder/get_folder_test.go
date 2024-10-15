bdcfd48304aa56fa697c297bd00705c5643be57d
Kushaagra Kesarwani <z5588175@ad.unsw.edu.au>
2024-10-15 22:19:44 +1100

    Created Unit Tests + Benchmark Tests For GetFoldersByOrgID
    Ensures accurate implementation and speed of the
    function

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -5,25 +5,90 @@ import (
 
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
-	// "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 // feel free to change how the unit test is structured
 func Test_folder_GetFoldersByOrgID(t *testing.T) {
 	t.Parallel()
-	tests := [...]struct {
+
+	// Sample test data
+	testData := folder.GetSampleData()
+	defaultOrgId := uuid.FromStringOrNil(folder.DefaultOrgID)
+
+	// Check case where input is valid
+	f := folder.NewDriver(testData)
+	res := f.GetFoldersByOrgID(defaultOrgId)
+
+	// Verify correct output for realistic data set
+	for _, folder := range res {
+		assert.Equal(t, folder.OrgId, defaultOrgId,
+			"Folders with invalid incorrect OrgId were returned")
+	}
+
+	errorTests := [...]struct {
 		name    string
 		orgID   uuid.UUID
 		folders []folder.Folder
 		want    []folder.Folder
 	}{
-		// TODO: your tests here
+		{
+			name:    "Expect empty: Invalid OrgId Provided",
+			orgID:   uuid.Must(uuid.NewV4()),
+			folders: testData,
+			want:    []folder.Folder{},
+		},
+		{
+			name:    "Expect empty: Empty folders input",
+			orgID:   uuid.Must(uuid.NewV4()),
+			folders: []folder.Folder{},
+			want:    []folder.Folder{},
+		},
+		// Path is irrelevant so can be ommitted
+		{
+			name:  "Expect output: Single Folder given",
+			orgID: defaultOrgId,
+			folders: []folder.Folder{
+				{OrgId: defaultOrgId, Name: "Folder 1"},
+			},
+			want: []folder.Folder{
+				{OrgId: defaultOrgId, Name: "Folder 1"},
+			},
+		},
+		{
+			name:  "Expect output: Multiple folders given",
+			orgID: defaultOrgId,
+			folders: []folder.Folder{
+				{OrgId: defaultOrgId, Name: "Folder 1"},
+				{OrgId: uuid.Must(uuid.NewV4()), Name: "Folder 2"},
+				{OrgId: defaultOrgId, Name: "Folder 3"},
+			},
+			want: []folder.Folder{
+				{OrgId: defaultOrgId, Name: "Folder 1"},
+				{OrgId: defaultOrgId, Name: "Folder 3"},
+			},
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// f := folder.NewDriver(tt.folders)
-			// get := f.GetFoldersByOrgID(tt.orgID)
 
+	for _, tt := range errorTests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := folder.NewDriver(tt.folders)
+			res := f.GetFoldersByOrgID(tt.orgID)
+			assert.Equal(t, res, tt.want, tt.name) // Checks if output == expected
 		})
 	}
 }
+
+func Benchmark_folder_GetFoldersByOrgID(b *testing.B) {
+	testData := folder.GetSampleData()
+	defaultOrgId := uuid.FromStringOrNil(folder.DefaultOrgID)
+	f := folder.NewDriver(testData)
+
+	for i := 0; i < b.N; i++ {
+		f.GetFoldersByOrgID(defaultOrgId)
+	}
+}
+
+// TODO: write tests for GetAllChildFolders
+//	Have coverage testing as well
+//  Have benchmark testing
