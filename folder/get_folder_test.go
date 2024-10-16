6746f8d06982d8b0b251b96610c324e96b1c530b
Kushaagra Kesarwani <z5588175@ad.unsw.edu.au>
2024-10-16 19:05:55 +1100

    Update GetAllChildFolders To Return Error And Wrote Unit Tests For GetAllChildFolders
    Allows for improved troubleshooting

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -89,6 +89,193 @@ func Benchmark_folder_GetFoldersByOrgID(b *testing.B) {
 	}
 }
 
-// TODO: write tests for GetAllChildFolders
-//	Have coverage testing as well
-//  Have benchmark testing
+func Test_folder_GetAllChildFolders(t *testing.T) {
+	t.Parallel()
+	defaultOrgId := uuid.FromStringOrNil(folder.DefaultOrgID)
+	altOrgID := uuid.Must(uuid.NewV4())
+
+	var testData = []folder.Folder{
+		{
+			Name:  "alpha",
+			Paths: "alpha",
+			OrgId: defaultOrgId,
+		},
+		{
+			Name:  "bravo",
+			Paths: "alpha.bravo",
+			OrgId: defaultOrgId,
+		},
+		{
+			Name:  "charlie",
+			Paths: "alpha.bravo.charlie",
+			OrgId: defaultOrgId,
+		},
+		{
+			Name:  "delta",
+			Paths: "alpha.delta",
+			OrgId: defaultOrgId,
+		},
+		{
+			Name:  "delta",
+			Paths: "alpha.delta.bravo.delta",
+			OrgId: defaultOrgId,
+		},
+		{
+			Name:  "delta",
+			Paths: "alpha.echo.delta",
+			OrgId: defaultOrgId,
+		},
+		{
+			Name:  "echo",
+			Paths: "echo",
+			OrgId: defaultOrgId,
+		},
+		{
+			Name:  "foxtrot",
+			Paths: "foxtrot",
+			OrgId: altOrgID,
+		},
+	}
+
+	tests := [...]struct {
+		name    string
+		orgID   uuid.UUID
+		folders []folder.Folder
+		want    []folder.Folder
+		wantErr bool
+	}{
+		// Expect output: When 1 folder matches criteria
+		{
+			name:    "echo",
+			orgID:   defaultOrgId,
+			folders: testData,
+			want: []folder.Folder{
+				{
+					Name:  "delta",
+					Paths: "alpha.echo.delta",
+					OrgId: defaultOrgId,
+				},
+			},
+			wantErr: false,
+		},
+		// Expect output: When multiple folders match criteria
+		{
+			name:    "alpha",
+			orgID:   defaultOrgId,
+			folders: testData,
+			want: []folder.Folder{
+				{
+					Name:  "bravo",
+					Paths: "alpha.bravo",
+					OrgId: defaultOrgId,
+				},
+				{
+					Name:  "charlie",
+					Paths: "alpha.bravo.charlie",
+					OrgId: defaultOrgId,
+				},
+				{
+					Name:  "delta",
+					Paths: "alpha.delta",
+					OrgId: defaultOrgId,
+				},
+				{
+					Name:  "delta",
+					Paths: "alpha.delta.bravo.delta",
+					OrgId: defaultOrgId,
+				},
+				{
+					Name:  "delta",
+					Paths: "alpha.echo.delta",
+					OrgId: defaultOrgId,
+				},
+			},
+			wantErr: false,
+		},
+		// Expect output: When name is in upper case
+		{
+			name:    "EcHo",
+			orgID:   defaultOrgId,
+			folders: testData,
+			want: []folder.Folder{
+				{
+					Name:  "delta",
+					Paths: "alpha.echo.delta",
+					OrgId: defaultOrgId,
+				},
+			},
+			wantErr: false,
+		},
+		// Expect output: When the folder name repeats in the path at the leaf
+		{
+			name:    "delta",
+			orgID:   defaultOrgId,
+			folders: testData,
+			want: []folder.Folder{
+				{
+					Name:  "delta",
+					Paths: "alpha.delta.bravo.delta",
+					OrgId: defaultOrgId,
+				},
+			},
+			wantErr: false,
+		},
+		// Expect Empty: When leaf folder is requested
+		{
+			name:    "foxtrot",
+			orgID:   altOrgID,
+			folders: testData,
+			want:    []folder.Folder{},
+			wantErr: false,
+		},
+		// Expect Error: When invalid folder is requested
+		{
+			name:    "invalid_folder",
+			orgID:   defaultOrgId,
+			folders: testData,
+			want:    []folder.Folder{},
+			wantErr: true,
+		},
+		// Expect Error: When folder doesn't exist in the specified organisation
+		{
+			name:    "foxtrot",
+			orgID:   defaultOrgId,
+			folders: testData,
+			want:    []folder.Folder{},
+			wantErr: true,
+		},
+		// Expect Error: When invalid OrgId is provided
+		{
+			name:    "alpha",
+			orgID:   uuid.Must(uuid.NewV4()),
+			folders: testData,
+			want:    []folder.Folder{},
+			wantErr: true,
+		},
+		// Expect Error: When no folders exist
+		{
+			name:    "alpha",
+			orgID:   defaultOrgId,
+			folders: []folder.Folder{},
+			want:    []folder.Folder{},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := folder.NewDriver(tt.folders)
+			res, err := f.GetAllChildFolders(tt.orgID, tt.name)
+
+			if tt.wantErr {
+				assert.Error(t, err, "Expected an error but got nil\n\n")
+			} else {
+				assert.NoError(t, err, "Did not expect an error but got one\n\n")
+			}
+
+			if !tt.wantErr {
+				assert.Equal(t, tt.want, res, "Unexpected result returned\n\n")
+			}
+		})
+	}
+}
