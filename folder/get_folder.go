cbdb8ef2de98b0d5055fb8639f3b8666f20399a7
Kushaagra Kesarwani <z5588175@ad.unsw.edu.au>
2024-10-16 21:49:33 +1100

    Implemented GetAllChildFolders
    All unit tests pass

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -1,6 +1,11 @@
 package folder
 
-import "github.com/gofrs/uuid"
+import (
+	"errors"
+	"strings"
+
+	"github.com/gofrs/uuid"
+)
 
 func GetAllFolders() []Folder {
 	return GetSampleData()
@@ -20,8 +25,47 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 
 }
 
-func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
-	// Your code here...
+// Checks if a particular folder exists
+func DoesFolderExist(folders []Folder, name string) bool {
+	for _, folder := range folders {
+		// Check if the folder.Paths contains the name
+		if strings.Contains(folder.Paths, name) {
+			return true
+		}
+	}
 
-	return []Folder{}, nil
+	return false
+}
+
+// Checks if a particular folder has child folders
+func HasSubFolders(Path string, name string) bool {
+	return strings.Contains(Path, name+".")
+}
+
+func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
+	name = strings.ToLower(name)
+	folders := f.GetFoldersByOrgID(orgID)
+	res := []Folder{}
+
+	if len(f.folders) == 0 {
+		return res, errors.New("no folders exist")
+	}
+
+	if len(folders) == 0 {
+		return res, errors.New("no such orgid exists")
+	}
+
+	if !DoesFolderExist(f.folders, name) {
+		return res, errors.New("invalid folder requested")
+	} else if !DoesFolderExist(folders, name) {
+		return res, errors.New("folder doesn't exist in specified organisation")
+	}
+
+	for _, folder := range folders {
+		if HasSubFolders(folder.Paths, name) {
+			res = append(res, folder)
+		}
+	}
+
+	return res, nil
 }
