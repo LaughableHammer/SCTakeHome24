a3d2ce00479de21f997ae83df1108316bed1be10
Kushaagra Kesarwani <z5588175@ad.unsw.edu.au>
2024-10-17 19:11:55 +1100

    Completed Implementation For MoveFolder

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -1,7 +1,52 @@
 package folder
 
-func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
-	// Your code here...
+import (
+	"errors"
+	"strings"
+)
 
-	return []Folder{}, nil
+func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
+	var srcFolder *Folder
+	var dstFolder *Folder
+
+	for i := range f.folders {
+		if f.folders[i].Name == name {
+			srcFolder = &f.folders[i]
+		}
+		if f.folders[i].Name == dst {
+			dstFolder = &f.folders[i]
+		}
+	}
+
+	if srcFolder == nil {
+		return []Folder{}, errors.New("source folder does not exist")
+	}
+
+	if dstFolder == nil {
+		return []Folder{}, errors.New("destination folder does not exist")
+	}
+
+	if srcFolder.Name == dstFolder.Name {
+		return []Folder{}, errors.New("cannot move a folder to itself")
+	}
+
+	if strings.HasPrefix(dstFolder.Paths, srcFolder.Paths+".") {
+		return []Folder{}, errors.New("cannot move a folder to a child of itself")
+	}
+
+	if srcFolder.OrgId != dstFolder.OrgId {
+		return []Folder{}, errors.New("cannot move a folder to a different organization")
+	}
+
+	// Update the paths for the source folder and its subfolders
+	oldPath := srcFolder.Paths
+	newPath := dstFolder.Paths + "." + srcFolder.Name
+
+	for i := range f.folders {
+		if strings.HasPrefix(f.folders[i].Paths, oldPath) {
+			f.folders[i].Paths = strings.Replace(f.folders[i].Paths, oldPath, newPath, 1)
+		}
+	}
+
+	return f.folders, nil
 }
